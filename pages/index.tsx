0553bddb73e9cf8a68f7cc04cd1397713615a439
Guilherme Magno <guilhermertfmagno@gmail.com>
2022-09-03 16:57:05 -0300

    feat: add frontend authentication using JWT and axios interceptors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,40 @@
+import { FormEvent, useContext, useState } from "react";
 import type { NextPage } from "next";
+
 import styles from "../styles/Home.module.css";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Home: NextPage = () => {
+  const [email, setEmail] = useState("diego@rocketseat.team");
+  const [password, setPassword] = useState("123456");
+
+  const { signIn } = useContext(AuthContext);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  }
+
   return (
-    <div className={styles.container}>
-      <h1>Hello World</h1>
-    </div>
+    <form className={styles.container} onSubmit={(e) => handleSubmit(e)}>
+      <input
+        type="email"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={({ target }) => setPassword(target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </form>
   );
 };
 
